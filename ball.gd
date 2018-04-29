65d72745a63649edd4aa871f6db9f765de0b0260
Martin Jablečník <martin.jablecnik@email.cz>
2018-04-29 16:17:10 +0200

    fix bouncing of ball

--- ball.gd
+++ ball.gd
@@ -1,28 +1,33 @@
 extends KinematicBody2D
 
 var motion = Vector2(0,0)
-var direction = 60
-var speed = 100
-var enemies = []
+var direction = 45
+var speed = 250
+var bricks = []
+var dir = Vector2(1,-1)
 
-func _ready():
-	direction = Vector2(0,0)
-	
 
 func _physics_process(delta):
-	motion.x = cos(direction * (PI/180))  * speed * delta
-	motion.y = -sin(direction * (PI/180)) * speed * delta
-	print(motion)
+	motion.x = dir.x * cos(direction * (PI/180))  * speed * delta
+	motion.y = dir.y * sin(direction * (PI/180)) * speed * delta
 	
 	var collide = move_and_collide(motion)
 	
-	enemies = get_tree().get_nodes_in_group("enemies")
+	bricks = get_tree().get_nodes_in_group("enemies")
 	
 	if collide:
-		motion = motion.bounce(collide.normal)
-		
-		for enemy in enemies:
-			if(collide.collider_id == enemy.get_instance_id()):
+		match Vector2(round(collide.normal.x), round(collide.normal.y)):
+			Vector2(0, 1):
+				dir.y = 1
+			Vector2(1, 0):
+				dir.x = 1
+			Vector2(0, -1):
+				dir.y = -1
+			Vector2(-1, 0):
+				dir.x = -1
+			
+		for brick in bricks:
+			if(collide.collider_id == brick.get_instance_id()):
 				instance_from_id(collide.collider_id).free()
 		
 		
