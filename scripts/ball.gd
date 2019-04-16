a8fa50112a50c74f44b7de95c56ac2bf2f14d54a
Martin Jablecnik <martin.jablecnik@email.cz>
2019-04-16 21:02:05 +0200

    refactoring and added highscore

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -15,51 +15,51 @@ var collide = false
 var start_position 
 
 func _ready():
-	start_position = position
-	randomize()
+    start_position = position
+    randomize()
 
 func _physics_process(delta):
-	if start:
-		speed += 0.1
-		
-		motion.x = dir.x * cos(direction * (PI/180)) * speed * delta
-		motion.y = dir.y * sin(direction * (PI/180)) * speed * delta
-		
-		collide = move_and_collide(motion)
-	
-	bricks = get_tree().get_nodes_in_group("bricks")
-	if len(bricks) == 0:
-		get_parent().next_level()
-	
-	if collide:
-		match Vector2(round(collide.normal.x), round(collide.normal.y)):
-			Vector2(0, 1):
-				dir.y = 1
-			Vector2(1, 0):
-				dir.x = 1
-			Vector2(0, -1):
-				dir.y = -1
-			Vector2(-1, 0):
-				dir.x = -1
-			
-		for brick in bricks:
-			if(collide.collider_id == brick.get_instance_id()):
-				var actual_score = int(get_parent().find_node("ControlPanel").find_node("ScoreText").text) 
-				actual_score += instance_from_id(collide.collider_id).SCORE_VALUE
-				get_parent().find_node("ControlPanel").find_node("ScoreText").text = str(actual_score)
-				instance_from_id(collide.collider_id).free()
-		
-		
+    if start:
+        speed += 0.1
+        
+        motion.x = dir.x * cos(direction * (PI/180)) * speed * delta
+        motion.y = dir.y * sin(direction * (PI/180)) * speed * delta
+        
+        collide = move_and_collide(motion)
+    
+    bricks = get_tree().get_nodes_in_group("bricks")
+    if len(bricks) == 0:
+        get_parent().next_level()
+    
+    if collide:
+        match Vector2(round(collide.normal.x), round(collide.normal.y)):
+            Vector2(0, 1):
+                dir.y = 1
+            Vector2(1, 0):
+                dir.x = 1
+            Vector2(0, -1):
+                dir.y = -1
+            Vector2(-1, 0):
+                dir.x = -1
+            
+        for brick in bricks:
+            if(collide.collider_id == brick.get_instance_id()):
+                var actual_score = int(get_parent().find_node("ControlPanel").find_node("ScoreText").text) 
+                actual_score += instance_from_id(collide.collider_id).SCORE_VALUE
+                get_parent().find_node("ControlPanel").find_node("ScoreText").text = str(actual_score)
+                instance_from_id(collide.collider_id).free()
+        
+        
 func _process(delta):
-	if self.position.y > 600:
-		get_parent().emit_signal("ball_out_of_room")
-		
+    if self.position.y > 600:
+        get_parent().emit_signal("ball_out_of_room")
+        
 
 func set_start_position():
-	position = start_position
-	motion = Vector2(0,0)
-	randomize()
-	dir = dir_chooses[randi() % 4]
-	previous_speed = speed
-	start = false
-	
+    position = start_position
+    motion = Vector2(0,0)
+    randomize()
+    dir = dir_chooses[randi() % 4]
+    previous_speed = speed
+    start = false
+    
--- scripts/game.gd
+++ scripts/game.gd
@@ -15,100 +15,100 @@ var adRewardVideoId = "ca-app-pub-3940256099942544/5224354917"
 var timer = Timer.new()
 
 func _ready():
-	Engine.time_scale = 0.8
-	connect("ball_out_of_room", self, "ball_out_of_room")
-	$Buttons/AddLiveButton.hide()
-	
-	if(Engine.has_singleton("AdMob")):
-		admob = Engine.get_singleton("AdMob")
-		admob.init(isReal, get_instance_id())
-		print(admob.loadBanner(adBannerId, isTop))
-		admob.loadRewardedVideo(adRewardVideoId)
-		admob.showBanner()
+    Engine.time_scale = 0.8
+    connect("ball_out_of_room", self, "ball_out_of_room")
+    $Buttons/AddLiveButton.hide()
+    
+    if(Engine.has_singleton("AdMob")):
+        admob = Engine.get_singleton("AdMob")
+        admob.init(isReal, get_instance_id())
+        print(admob.loadBanner(adBannerId, isTop))
+        admob.loadRewardedVideo(adRewardVideoId)
+        admob.showBanner()
 
 
 func _process(delta):
-	if Input.is_action_just_released("enter"):
-		$Buttons/StartButton.emit_signal("pressed")
+    if Input.is_action_just_released("enter"):
+        $Buttons/StartButton.emit_signal("pressed")
 
 func setup_timer():
-	self.timer.wait_time = TIMER_WAIT_TIME
-	self.timer.one_shot = true 
-	add_child(self.timer)
-	self.timer.start()
-	
+    self.timer.wait_time = TIMER_WAIT_TIME
+    self.timer.one_shot = true 
+    add_child(self.timer)
+    self.timer.start()
+    
 
 func _on_StartButton_pressed():
-	$Buttons.hide()
-	if $ball != null:
-		print("start Game")
-		$ball.direction = 45
-		$ball.speed = $ball.previous_speed
-		$ball.start = true
-		$pad.start = true
-		setup_timer()
-	else:
-		print("restart Game")
-		restart()
-		
+    $Buttons.hide()
+    if $ball != null:
+        print("start Game")
+        $ball.direction = 45
+        $ball.speed = $ball.previous_speed
+        $ball.start = true
+        $pad.start = true
+        setup_timer()
+    else:
+        print("restart Game")
+        restart()
+        
 func load_new_bricks():
-	var resource = "res://scenes/level%d.tscn" % level
-	var scene = load(resource)
-	var bricks = scene.instance()
-	bricks.position = Vector2(0,0)
-	bricks.z_index = -1
-	add_child(bricks)
-	
-	
+    var resource = "res://scenes/level%d.tscn" % level
+    var scene = load(resource)
+    var bricks = scene.instance()
+    bricks.position = Vector2(0,0)
+    bricks.z_index = -1
+    add_child(bricks)
+    
+    
 func next_level():
-	level += 1
-	if level > MAX_LEVELS:
-		level = 1
-	$pad.speed = $pad.START_SPEED
-	$ball.speed = $ball.START_SPEED
-	$ball.set_start_position()
-	$pad.set_start_position()
-	$ControlPanel/lives.add_live()
-	load_new_bricks()
-	$Buttons.show()
-	$Buttons/AddLiveButton.hide()
-	#load_reward_video()
-	
+    level += 1
+    if level > MAX_LEVELS:
+        level = 1
+    $pad.speed = $pad.START_SPEED
+    $ball.speed = $ball.START_SPEED
+    $ball.set_start_position()
+    $pad.set_start_position()
+    $ControlPanel/lives.add_live()
+    load_new_bricks()
+    $Buttons.show()
+    $Buttons/AddLiveButton.hide()
+    #load_reward_video()
+    
 func load_reward_video():
-	if admob != null and self.timer.get_time_left() != 0:
-		admob.loadRewardedVideo(adRewardVideoId)
-		
+    if admob != null and self.timer.get_time_left() != 0:
+        admob.loadRewardedVideo(adRewardVideoId)
+        
 func _on_rewarded_video_ad_loaded():
-	$Buttons/AddLiveButton.show()
-		
+    $Buttons/AddLiveButton.show()
+        
 func _on_rewarded_video_ad_failed_to_load(errorCode):
-	$Buttons/AddLiveButton.hide()
-	
+    $Buttons/AddLiveButton.hide()
+    
 func show_game_over():
-	$pad.start = false
-	$GameOver.visible = true
-	
+    $pad.start = false
+    $GameOver.visible = true
+    
 func restart():
-	get_tree().change_scene("res://scenes/game.tscn")
+    get_tree().change_scene("res://scenes/game.tscn")
 
 
 func ball_out_of_room():
-	$ControlPanel/lives.remove_live()
-	if $ControlPanel/lives.get_lives_num() <= 0:
-		show_game_over()
-		$ball.free()
-	else:
-		$ball.set_start_position()
-		$pad.set_start_position()
-		$Buttons.show()
-		load_reward_video()
-		
+    $ControlPanel/lives.remove_live()
+    if $ControlPanel/lives.get_lives_num() <= 0:
+        show_game_over()
+        $ball.free()
+    else:
+        $ball.set_start_position()
+        $pad.set_start_position()
+        $Buttons.show()
+        load_reward_video()
+        
 
 
 func _on_AddLiveButton_pressed():
-	if admob != null:
-		admob.showRewardedVideo()
+    if admob != null:
+        admob.showRewardedVideo()
 
 func _on_rewarded_video_ad_closed():
-	$Buttons/AddLiveButton.hide()
-	$ControlPanel/lives.add_live()
+    $Buttons/AddLiveButton.hide()
+    $ControlPanel/lives.add_live()
